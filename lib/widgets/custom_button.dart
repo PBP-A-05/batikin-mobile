feb086e30f69db5c05d56d5fb362f732ee26ceca
denisfilab <daanish.inayat@ui.ac.id>
2024-12-02 20:23:38 +0700

    feat: restructure project

---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,60 @@
+// lib/widgets/custom_button.dart
+import 'package:flutter/material.dart';
+import 'package:batikin_mobile/constant/colors.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+  final Color textColor;
+  final EdgeInsetsGeometry padding;
+  final double borderRadius;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final double elevation;
+  final double? width;
+  final double? height;
+
+  const CustomButton({
+    Key? key,
+    required this.text,
+    required this.onPressed,
+    this.backgroundColor = AppColors.coklat3,
+    this.textColor = Colors.white,
+    this.padding = const EdgeInsets.symmetric(vertical: 16.0, horizontal: 24.0),
+    this.borderRadius = 12.0,
+    this.fontSize = 16.0,
+    this.fontWeight = FontWeight.w600,
+    this.elevation = 2.0,
+    this.width,
+    this.height,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: width ?? double.infinity, // Default to full width if not provided
+      height: height ?? 50.0, // Default height if not provided
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: backgroundColor,
+          foregroundColor: textColor,
+          padding: padding,
+          elevation: elevation,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(borderRadius),
+          ),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: fontSize,
+            fontWeight: fontWeight,
+            color: textColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
